1d8e7760c44c86172606ef21ea0e86e871849812
mostafahagras <mostafahagras07@gmail.com>
2024-05-13 18:43:00 +0300

    Add logo to navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,15 +1,25 @@
 import { ThemeToggle } from "@/components/theme-toggle";
 import Link from "next/link";
-import {} from "lucide-react";
+import Image from "next/image";
 
 export default function Navbar() {
 	return (
-		<header className="sticky top-0 bg-transparent">
+		<header className="sticky top-0 z-10 bg-transparent">
 			<div className="flex justify-center bg-background/85 px-6 saturate-[1.8] backdrop-blur-sm py-3 border-b">
 				<div className="flex max-w-7xl flex-1 items-center">
 					<div className="flex flex-1 items-center">
-						<Link className="flex" href="/" title="Go to the homepage">
-							{/* <School /> */}
+						<Link
+							className="flex items-center"
+							href="/"
+							title="Go to the homepage"
+						>
+							<Image
+								src="/bp.png"
+								width={30}
+								height={30}
+								alt="Logo"
+								className="-translate-y-[3px]"
+							/>
 							<div className="text-xl flex-nowrap font-medium">
 								Chess Openings
 							</div>
