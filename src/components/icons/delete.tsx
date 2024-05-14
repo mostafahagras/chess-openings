aad387643448690d1c501864fd38ca394a912fa7
mostafahagras <mostafahagras07@gmail.com>
2024-05-14 16:09:04 +0300

    Add some icons in src/components/icons

---
+++ src/components/icons/delete.tsx
@@ -0,0 +1,12 @@
+export default function Delete() {
+	return (
+		<svg
+			className="inline-block size-6 shrink-0 select-none fill-current"
+			focusable="false"
+			aria-hidden="true"
+			viewBox="0 0 24 24"
+		>
+			<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z" />
+		</svg>
+	);
+}
---
+++ src/components/icons/external-link.tsx
@@ -0,0 +1,12 @@
+export default function ExternalLink() {
+	return (
+		<svg
+			className="inline-block size-6 shrink-0 select-none fill-current"
+			focusable="false"
+			aria-hidden="true"
+			viewBox="0 0 24 24"
+		>
+			<path d="M6 6v2h8.59L5 17.59 6.41 19 16 9.41V18h2V6z" />
+		</svg>
+	);
+}
