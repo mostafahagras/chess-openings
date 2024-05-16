163ab049e5fb9de147d0c05c31797bd59ca57d47
mostafahagras <mostafahagras07@gmail.com>
2024-05-16 20:14:47 +0300

    Add an alert to let the users know the app is still under development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import Link from "next/link";
 import Openings from "./openings";
 
 export default function Home() {
 	return (
 		<main className="p-4">
 			<Openings previousMoves={[]} />
+			<Alert variant="warning" className="mt-4">
+				<AlertTitle>Important</AlertTitle>
+				<AlertDescription>
+					This app is under development and is constantly changing. for any
+					issues, Please submit them{" "}
+					<Link
+						href="https://github.com/mostafahagras/chess-openings/issues/new"
+						target="_blank"
+						className="underline"
+					>
+						here
+					</Link>
+				</AlertDescription>
+			</Alert>
 		</main>
 	);
 }
